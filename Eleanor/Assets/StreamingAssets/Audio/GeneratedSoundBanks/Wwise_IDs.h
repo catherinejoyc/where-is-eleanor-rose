1967c6b545e90bc91f1cc8cfdb623d66e680f6ed
DilpreetSN <dilpreetsingh.n@gmail.com>
2021-02-05 20:23:19 +0100

    removed unessecary plugins from wwise projects

--- Eleanor/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Eleanor/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -108,19 +108,7 @@ namespace AK
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID LOWPASS_RTPC = 1119582304U;
-        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
         static const AkUniqueID PLAYERLOCATION_RTPC = 332625859U;
-        static const AkUniqueID RPM = 796049864U;
-        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
-        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
-        static const AkUniqueID SS_AIR_FURY = 1029930033U;
-        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
-        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
-        static const AkUniqueID SS_AIR_RPM = 822163944U;
-        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
-        static const AkUniqueID SS_AIR_STORM = 3715662592U;
-        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
-        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
     } // namespace GAME_PARAMETERS
 
     namespace TRIGGERS
@@ -137,13 +125,11 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
         static const AkUniqueID MUSIC_BUS = 3127962312U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
     {
-        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
         static const AkUniqueID NO_OUTPUT = 2317455096U;
         static const AkUniqueID SYSTEM = 3859886410U;
     } // namespace AUDIO_DEVICES
